5401145439db3f9eab69ddab66cc1feba4bf45a5
Kevin Martínez <kevinmartinezcto@gmail.com>
2023-11-11 13:34:45 -0300

    First Commit

---
+++ app.js
@@ -0,0 +1,28 @@
+let weather = {
+    "apiKey": "85c21654651478841d7d436b03b27a14",
+    fetchWeather: function (city) {
+        fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + this.apiKey + "&units=metric").then((response) => response.json()).then((data) => this.displayWeather(data))
+    },
+    displayWeather: function (data) {
+        const { name } = data;
+        const { icon, description } = data.weather[0];
+        const { temp, humidity } = data.main;
+        const { speed } = data.wind;
+        console.log(name, icon, description, temp, humidity, speed);
+
+        document.querySelector(".city").innerText = "Weather in" + " " + name;
+        document.querySelector(".icon").src = "https://openweathermap.org/img/wn/"+ icon +"@2x.png";
+        document.querySelector(".temp").innerText = temp + "°C";
+        document.querySelector(".description").innerText = description;
+        document.querySelector(".humidity").innerText = "Humidity:" + " " + humidity + "%";
+        document.querySelector(".wind").innerText = "Wind speed:" + " " + speed + " " + "Km/h";
+
+    },
+    search: function () {
+        this.fetchWeather(document.querySelector(".search-bar").value)
+    },
+};
+
+document.querySelector(".search button").addEventListener("click", function () {
+    weather.search();
+});
