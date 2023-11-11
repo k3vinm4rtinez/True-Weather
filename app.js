a04396ba48afc54c693705865f454e7a41811528
Kevin Martínez <kevinmartinezcto@gmail.com>
2023-11-11 14:07:14 -0300

    Arreglos

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ let weather = {
         document.querySelector(".description").innerText = description;
         document.querySelector(".humidity").innerText = "Humidity:" + " " + humidity + "%";
         document.querySelector(".wind").innerText = "Wind speed:" + " " + speed + " " + "Km/h";
+        document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?" + name + "')";
 
     },
     search: function () {
@@ -25,4 +26,10 @@ let weather = {
 
 document.querySelector(".search button").addEventListener("click", function () {
     weather.search();
+});
+
+document.querySelector(".search-bar").addEventListener("keyup", function (event) {
+    if (event.key == "Enter") {
+        weather.search();
+    }
 });
